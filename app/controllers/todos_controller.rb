0cb3d56d7df394ba7a4a9adb5d853e9e26cc18d4
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-24 12:47:28 -0800

    comment out WIP for creating new todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,18 @@ class TodosController < ApplicationController
   def index
     @todo_items = Todo.all
   end
+
+   # def insert_new 
+   # if new_todo != nil
+   #   Todo.create(:todo_item => new_todo)
+   #   alert("created with #{new_todo}")
+   # else
+   #   alert("failed to create with #{new_todo}")
+   # end
+    
+    def delete
+      #put delete logic here
+    end
+
+  end
 end
