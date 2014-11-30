a7ef87785f37dc65d0dc15317909ca7084815cf8
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-29 22:32:17 -0800

    add error catching and displaying to todos_controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,7 +10,10 @@ class TodosController < ApplicationController
 
 
   def add
-    Todo.create(:todo_item => params[:todo_text])
+    todo = Todo.create(:todo_item => params[:todo_text])
+    if !todo.valid?
+      flash[:error] = todo.errors.full_messages.join("<br>").html_safe
+    end
     redirect_to :action => 'index'
   end
 
