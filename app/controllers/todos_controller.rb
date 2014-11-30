3700e37f040b3c57b940c1ef4b9f97ab9277a315
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-30 14:05:50 -0800

    add success message when creating new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,6 +13,8 @@ class TodosController < ApplicationController
     todo = Todo.create(:todo_item => params[:todo_text])
     if !todo.valid?  # or say unless todo.valid?
       flash[:error] = todo.errors.full_messages.join("<br>").html_safe
+else
+      flash[:success] = "You've added to the list!".html_safe     
     end
     redirect_to :action => 'index'
   end
