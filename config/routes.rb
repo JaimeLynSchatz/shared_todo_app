54de634c3b543433193a556dd4508dbb928902d2
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-11-29 21:22:41 -0800

    make links more rails-y

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'todos/index'
-  get '/todos/delete'
+  # remove get '/todos/delete' and replace with
+  match "todos/delete" => "todos#delete", :as => :delete
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
